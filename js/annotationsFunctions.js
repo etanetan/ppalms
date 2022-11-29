a1c3e21a403f9c4913e80e18b849a1af79ecc0ce
Ethan Myos <myos0004@umn.edu>
2022-11-28 19:57:59 -0600

    fixed clearLocalStorage() function

--- js/annotationsFunctions.js
+++ js/annotationsFunctions.js
@@ -16,6 +16,8 @@ function logJSON() {
 }
 
 // read file into array, initialize myObj, return array for display on annotation screen
+// This function traces back to the file selection design element
+// along with the scenario of a user selecting a file
 async function uploadFile() {
 	console.log(myObj);
 	let [FileHandle] = await window.showOpenFilePicker();
@@ -36,6 +38,8 @@ async function uploadFile() {
 }
 
 // changes included status of lineID
+// This function traces back to the file annotations design element
+// specifically, selecting which lines should be included in the final output
 function toggleIncluded(lineID) {
 	if (myObj.linesData[lineID].included) {
 		myObj.linesData[lineID].included = false;
@@ -45,6 +49,8 @@ function toggleIncluded(lineID) {
 }
 
 // Adds two lines to each other's relatedLines arrays
+// This function traces back to the file annotations design element
+// specifically, selecting which lines do not need to be in any specified order
 function addRelatedLines(ID1, ID2) {
 	myObj.linesData[ID1].relatedLineIDs.push(ID2);
 	myObj.linesData[ID2].relatedLineIDs.push(ID1);
@@ -52,6 +58,8 @@ function addRelatedLines(ID1, ID2) {
 
 // removes all entries from myObj where contents == "". Sets new lineIDs and clears the relatedLines array.
 // may be a helpful option for users
+// This function traces back to the file annotations design element
+// specifically, removing lines that only contain spaces in them
 function cleanEmptyContents() {
 	let tempObj = { problemType: null, exportMethod: null, linesData: [] };
 	const len = myObj.linesData.length;
@@ -77,11 +85,14 @@ function cleanEmptyContents() {
 	myObj = tempObj;
 }
 // resets object fields to what they were before annotating
+// This function traces back to the file annotations design element
+// specifically, resetting all annotations to the lines
 function resetSelection(){
     myObj = copyObj;
 }
 
-
+// This function traces back to the data storage design element
+// data is stored locally
 function save() {
 	// save the library to local storage
 	localStorage.setItem('lines', JSON.stringify(myObj));
@@ -100,7 +111,8 @@ if (theObj) {
 if (theCopyObj) {
     copyObj = theCopyObj;
 }
-
+// This function traces back to the data storage design element
+// data is cleared locally in this function
 function clearLocalStorage() {
 	console.log('cleared local storage');
 	localStorage.clear();
