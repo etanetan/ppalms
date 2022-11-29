cee913d780ce8f27be48a1711ef67da6924a8f54
Ethan Myos <myos0004@umn.edu>
2022-11-28 21:30:38 -0600

    testing file inputs

--- js/annotationsFunctions.js
+++ js/annotationsFunctions.js
@@ -1,17 +1,23 @@
 // represents a linesData entry
 class Line {
+    // constructor to set id and contents for line
 	constructor(id, contents) {
 		this.id = id;
 		this.contents = contents;
 	}
+    // line is included in output by default
 	included = true;
+    // array to hold the lines that it can be grouped with
 	relatedLineIDs = [];
 }
-
+// main object for holding the problem type, export method, and data
+// on all of the lines in the source file
 let myObj = { "problemType": null, "exportMethod": null, "linesData": [] };
+// copy object used for resetting the annotations on the lines
 let copyObj = { "problemType": null, "exportMethod": null, "linesData": [] };
 
 function logJSON() {
+    // log the json
 	console.log(JSON.stringify(myObj));
 }
 
@@ -23,26 +29,27 @@ async function uploadFile() {
 	let [FileHandle] = await window.showOpenFilePicker();
 	let fileData = await FileHandle.getFile();
 	let text = await fileData.text();
-	console.log('here is the text' + text);
 	let fileSpace = document.getElementById('fileSpace');
 	fileSpace.innerHTML = text;
 	const arr1 = text.split(/\r?\n/);
 	let arr2 = arr1.filter((a) => a != '');
 	for (let i = 0; i < arr2.length; i++) {
 		let l = new Line(i, arr2[i]); // create & add a line entry to myObj for each arr entry
-		console.log('loading data');
 		myObj.linesData.push(l);
 	}
-	console.log('Contents\n' + JSON.stringify(myObj));
+    // set the copy lines to be the lines at this point, with no annotations
+    // so it can later be reset to this exact state if requested by the user
 	copyObj = myObj;
 }
 
 // changes included status of lineID
 // This function traces back to the file annotations design element
-// specifically, selecting which lines should be included in the final output
+// specifically, toggling which lines should be included in the final output
 function toggleIncluded(lineID) {
+    // if line is currently included, do not include it
 	if (myObj.linesData[lineID].included) {
 		myObj.linesData[lineID].included = false;
+    // if line is currently not included, now include it
 	} else {
 		myObj.linesData[lineID].included = true;
 	}
@@ -50,48 +57,30 @@ function toggleIncluded(lineID) {
 
 // Adds ID2 to the relatedLines array of ID1 and all other arrays included in ID1
 // Then updates ID2's relatedArray 
-function addRelatedLines(ID1, ID2) {
-	let l = myObj.linesData[ID1].relatedLines.length; // length of related lines array
-	for (let i = 0; i < l; i++) {
-		let curID = myObj.linesData[ID1].relatedLines[i];
-		// if lineID not already in array, push
-		if (myObj.linesData[curID].relatedLineIDs.indexOf(ID2) == -1) { 
-			myObj.linesData[curID].relatedLineIDs.push(ID2);
-		}
-	}
-	// add ID2 to ID1's array then copy into ID2's array
-	myObj.linesData[ID1].relatedLineIDs.push(ID2);
-	myObj.linesData[ID2].relatedLineIDs = myObj.linesData[ID1].relatedLineIDs;
-	// replace ID of ID2 in ID2's array with the ID of ID1
-	let ownIndex = myObj.linesData[ID2].relatedLines.indexOf(ID2);
-	myObj.linesData[ID2].relatedLines[ownIndex] = ID1;
-}
-
 function addRelatedLinesV2(ID1, ID2) {
 	let a1 = myObj.linesData[ID1].relatedLines;
 	let a2 = myObj.linesData[ID2].relatedLines;
-	let arr = [...a1, ...a2];  // combined arrays
+	let temp = [...a1, ...a2];  // combined arrays
+	// make every element in array unique:
+	var arr = temp.filter(uniqueElements);
 	let l = arr.length;
-	let lineInd = -1;
 
 	for(let i=0;i<l;i++){
-		let curID = arr[i];
-		// get index into linesData of current line
-		for(let j=0;j<myObj.linesData.length;j++){
-			if(myObj.linesData[j].id == curID){
-				lineInd = j;
-				break;
-			}
-		}
-		if(lineInd != -1){
-			if (myObj.linesData[lineInd].relatedLineIDs.indexOf(curID) != -1) { 
-				myObj.linesData[lineInd].splice(arr[i], arr[i]);
-			}
+		// current line id
+		cur = arr[i];
+		// set array of current item to the combined array
+		myObj.linesData[cur].relatedLineIDs = arr;
+		// remove self's id from array:
+		let ind = myObj.linesData[cur].relatedLineIDs.indexOf(cur);
+		if(ind != -1){  // if self's ID is in array, remove
+			myObj.linesData[i].relatedLineIDs.splice(ind, ind);
 		}
 	}
-
 }
-
+// helper function for addRelatedLines to find duplicate array entries
+function uniqueElements(value, index, self) {
+	return self.indexOf(value) === index;
+  }
 // removes all entries from myObj where contents == "". Sets new lineIDs and clears the relatedLines array.
 // may be a helpful option for users
 // This function traces back to the file annotations design element
@@ -130,38 +119,49 @@ function resetSelections() {
 // This function traces back to the data storage design element
 // data is stored locally
 function save() {
-	// save the library to local storage
+    // store the main object lines to local storage
 	localStorage.setItem('lines', JSON.stringify(myObj));
+    // store a copy of the object to local storage
 	localStorage.setItem('copyLines', JSON.stringify(copyObj));
-	alert('saved to local storage');
 }
-
+// parse the json to find the main object lines in local storage
 let theObj = JSON.parse(localStorage.getItem('lines'));
-
+// parse the json to find the copy object lines in local storage
 let theCopyObj = JSON.parse(localStorage.getItem('copyLines'));
 
 console.log('retrieved item: ' + theObj);
+// if the main object was in local storage, set the main object to 
+// be equal to what was found in storage
 if (theObj) {
 	myObj = theObj;
 }
+// if the copy object was in local storage, set the copy object to 
+// be equal to what was found in storage
 if (theCopyObj) {
 	copyObj = theCopyObj;
 }
 // This function traces back to the data storage design element
 // data is cleared locally in this function
 function clearLocalStorage() {
-	console.log('cleared local storage');
+    // clear local storage
 	localStorage.clear();
 }
 // This function traces back to the file annotations design element
 // specifically, adding buttons for each of the lines in the file
 function addAnnotationLines() {
+    // grab the area to place the buttons
 	let displayArea = document.getElementById('displayContents');
+    // loop through the lines
 	for (let i = 0; i < myObj.linesData.length; i++) {
+        // create a button for each line
 		let button = document.createElement('button');
+        // add a unique line id
 		button.setAttribute('id', 'lineButton-' + i.toString());
+        // add a class to style the buttons
         button.classList.add('lineButton');
+        // set the text to be the same as the line text
 		button.innerHTML = myObj.linesData[i].contents;
+        // add the button to the correct area on the page
 		displayArea.appendChild(button);
 	}
 }
